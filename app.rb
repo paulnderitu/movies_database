a6b6bc128d639ac536172d64c0b7bf810e98ac9f
Your Name <youremail@domain.com>
2017-07-13 13:09:53 +0300

    required movie database in the app.rb file

--- app.rb
+++ app.rb
@@ -4,3 +4,10 @@ require('./lib/movie')
 require('./lib/actor')
 also_reload('lib/**/*.rb')
 require("pg")
+
+DB = PG.connect({:dbname => "movie_database"})
+
+get("/")do
+# @patrons = Patron.all()
+  erb(:index)
+end

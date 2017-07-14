4c9782b336ef235cf7ae7b2b21b306bec37d446d
Your Name <youremail@domain.com>
2017-07-14 15:04:08 +0300

    added both actor & movie info.erb files to hold details for a particular actor and movie respectively

--- app.rb
+++ app.rb
@@ -22,3 +22,48 @@ require("sinatra")
     @movies = Movie.all()
     erb(:movies)
   end
+
+  post("/actors") do
+    name = params.fetch("name")
+    actor = Actor.new({:name => name, :id => nil})
+    actor.save()
+    @actors = Actor.all()
+    erb(:actors)
+  end
+
+  post("/movies") do
+    name = params.fetch("name")
+    movie = Movie.new({:name => name, :id => nil})
+    movie.save()
+    @movies = Movie.all()
+    erb(:movies)
+  end
+
+  get("/actors/:id") do
+    @actor = Actor.find(params.fetch("id").to_i())
+    @movies = Movie.all()
+    erb(:actor_info)
+  end
+
+  get("/movies/:id") do
+    @movie = Movie.find(params.fetch("id").to_i())
+    @actors = Actor.all()
+    erb(:movie_info)
+  end
+  patch("/actors/:id") do
+    actor_id = params.fetch("id").to_i()
+    @actor = Actor.find(actor_id)
+    movie_ids = params.fetch("movie_ids")
+    @actor.update({:movie_ids => movie_ids})
+    @movies = Movie.all()
+    erb(:actor_info)
+  end
+
+  patch("/movies/:id") do
+    movie_id = params.fetch("id").to_i()
+    @movie = Movie.find(movie_id)
+    actor_ids = params.fetch("actor_ids")
+    @movie.update({:actor_ids => actor_ids})
+    @actors = Actor.all()
+    erb(:movie_info)
+  end
